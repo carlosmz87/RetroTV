f827094d523e94692b5f8796399d136089f9e263
carlosmz87 <carlosmartz1995@gmail.com>
2023-05-16 20:24:33 -0600

    [ADD] Formulario de registro de usuarios y arreglos a la base de datos

--- frontend/src/app/componentes/registro/modulo-registro.module.ts
+++ frontend/src/app/componentes/registro/modulo-registro.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VistaRegistroComponent } from './vistas/vista-registro.component';
 import { ServicioRegistroService } from './servicios/servicio-registro.service';
-
-
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     VistaRegistroComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    ReactiveFormsModule
   ],
   exports: [
     VistaRegistroComponent
--- frontend/src/app/componentes/registro/vistas/vista-registro.component.ts
+++ frontend/src/app/componentes/registro/vistas/vista-registro.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators, Form} from '@angular/forms';
+
 
 @Component({
   selector: 'app-vista-registro',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./vista-registro.component.css']
 })
 export class VistaRegistroComponent {
-
+  FormularioRegistroCliente = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.pattern('[A-Za-z]+')]),
+    usuario: new FormControl('',[Validators.required, Validators.maxLength(10)]),
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    contrasena: new FormControl('', [Validators.required]),
+    conf_contrasena: new FormControl('', [Validators.required]),
+    telefono: new FormControl('', [Validators.required, Validators.pattern('^(\\d{3}-\\d{3}-\\d{4})|(\\d{3}-\\d{4}-\\d{4})$')]),
+    genero: new FormControl('', [Validators.required]),
+    fecha: new FormControl('', [Validators.required]),
+    suscripcion: new FormControl(false)
+  });
+  hide = true;
+  
+  formatPhone() {
+    const phoneNumber = this.FormularioRegistroCliente.get('telefono')?.value;
+    let formattedPhoneNumber = '';
+  
+    if (phoneNumber) {
+      formattedPhoneNumber = phoneNumber.replace(/\D/g, ''); // Elimina todos los caracteres no numéricos
+      if (formattedPhoneNumber.match(/^(\d{3})(\d{3})(\d{4})$/)) {
+        formattedPhoneNumber = formattedPhoneNumber.replace(/^(\d{3})(\d{3})(\d{4})$/, '$1-$2-$3');
+      } else if (formattedPhoneNumber.match(/^(\d{3})(\d{4})(\d{4})$/)) {
+        formattedPhoneNumber = formattedPhoneNumber.replace(/^(\d{3})(\d{4})(\d{4})$/, '$1-$2-$3');
+      }
+    }
+  
+    this.FormularioRegistroCliente.get('telefono')?.setValue(formattedPhoneNumber);
+  }
+  
+  RegistrarCliente(){
+    console.log(this.FormularioRegistroCliente.value)
+  }
 }
